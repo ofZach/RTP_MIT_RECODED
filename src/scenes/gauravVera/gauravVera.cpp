21a2d62c7aa54efc7220c9db797d9191c6079cf6
zach <zach@sfpc.io>
2019-10-29 15:48:23 -0400

    adding gaurav

---
+++ src/scenes/gauravVera/gauravVera.cpp
@@ -0,0 +1,23 @@
+
+#include "gauravVera.h"
+
+void gauravVera::setup(){
+  
+    // setup parameters
+    // if your original code use an ofxPanel instance dont use it here, instead
+    // add your parameters to the "parameters" instance as follows.
+    // param was declared in gauravVera.h
+    // parameters.add(param.set("param", 5, 0, 100));
+
+    setAuthor("Put Your Name Here");
+    setOriginalArtist("Put the original Artist's name here");
+
+    loadCode("scenes/gauravVera/exampleCode.cpp");
+}
+
+void gauravVera::update(){
+    
+}
+
+void gauravVera::draw(){
+}
