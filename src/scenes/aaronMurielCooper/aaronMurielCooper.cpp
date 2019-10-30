76e0886c8f88c80e52ec64d836b9bd18c5f41fd5
montoyamoraga <montoyamoraga@gmail.com>
2019-10-30 16:28:35 -0400

    add aaronMurielCooper

---
+++ src/scenes/aaronMurielCooper/aaronMurielCooper.cpp
@@ -0,0 +1,23 @@
+
+#include "aaronMurielCooper.h"
+
+void aaronMurielCooper::setup(){
+  
+    // setup parameters
+    // if your original code use an ofxPanel instance dont use it here, instead
+    // add your parameters to the "parameters" instance as follows.
+    // param was declared in aaronMurielCooper.h
+    // parameters.add(param.set("param", 5, 0, 100));
+
+    setAuthor("Put Your Name Here");
+    setOriginalArtist("Put the original Artist's name here");
+
+    loadCode("scenes/aaronMurielCooper/exampleCode.cpp");
+}
+
+void aaronMurielCooper::update(){
+    
+}
+
+void aaronMurielCooper::draw(){
+}
