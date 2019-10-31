bc16c7e18907f9d7d4a1cb18d6dd5e15d0f8f825
montoyamoraga <montoyamoraga@gmail.com>
2019-10-31 10:02:20 -0400

    add draft of aaronLillianSchwartz scene

---
+++ src/scenes/aaronLillianSchwartz/aaronLillianSchwartz.cpp
@@ -0,0 +1,23 @@
+
+#include "aaronLillianSchwartz.h"
+
+void aaronLillianSchwartz::setup(){
+  
+    // setup parameters
+    // if your original code use an ofxPanel instance dont use it here, instead
+    // add your parameters to the "parameters" instance as follows.
+    // param was declared in aaronLillianSchwartz.h
+    // parameters.add(param.set("param", 5, 0, 100));
+
+    setAuthor("Put Your Name Here");
+    setOriginalArtist("Put the original Artist's name here");
+
+    loadCode("scenes/aaronLillianSchwartz/exampleCode.cpp");
+}
+
+void aaronLillianSchwartz::update(){
+    
+}
+
+void aaronLillianSchwartz::draw(){
+}
