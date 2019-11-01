8f3bf7bdc31f9e9e22e8e2037f77674163fbcef8
Karsten Schuhl <mail@karstenschuhl.com>
2019-11-01 10:16:14 -0400

    added recordings

--- bin/data/scenes/karstenJohnMaeda/exampleCode.cpp
+++ bin/data/scenes/karstenJohnMaeda/exampleCode.cpp
@@ -6,46 +6,27 @@ void setup(){
 }
 
 void draw(){
-    
         float xBegin = 0.0;
         float yBegin = 0.0;
         float width = getWidth();
         float height = getHeight();
         int repeatLetters = 350;
-        setColor(245);
-        fill();
-        drawRectangle(0, 0, width, height);
-        setColor(0);
         string logo = "モリサワ"; //MO-RI-SA-WA
-        rectangle bounds = font.getStringBoundingBox(logo, 0.0, 0.0);
-        xBegin = (width-bounds.width)/2-10;
-        yBegin = (bounds.height+3);
-        noFill();
-        setLineWidth(1);
-    
         pushMatrix();
         translate(xBegin,yBegin);
         int character = paths.size();
         for (int i = 0; i < character; i++){
-            ofPath path = paths[i];
-            vector < ofPolyline > lines = path.getOutline();
             float xPos = 0.0;
             pushMatrix();
             for(int j = 0; j < repeatLetters; j++ ){
                 float yPos = (j+1)/9.2;
                 if(i >= character - 2){
-                    xPos = [[amp]] * sin(-j * [[freq]]) * noise(j* [[noiseFactor]], i * 1.39);
+                    xPos = [[amp]] * sin(-j * [[freq]]) * noise(j* [[noiseFactor]], i);
                 } else{
-                    xPos = [[amp]] * sin(j * [[freq]]) * noise(j* [[noiseFactor]], i * 0.047);
+                    xPos = [[amp]] * sin(j * [[freq]]) * noise(j* [[noiseFactor]], i);
                 }
                 translate(xPos, yPos);
                 rotate(noise([[rot]], i));
-                
-                for (int k = 0; k < lines.size(); k++){
-                    line line = lines[k];
-                    setColor(80, 80, 80, ofMap(yPos, 0, 16 , 255, 0));
-                    lines[k].draw();
-                }
             }
             popMatrix();
         }
--- bin/data/scenes/karstenVasulka/exampleCode.cpp
+++ bin/data/scenes/karstenVasulka/exampleCode.cpp
@@ -1,41 +1,28 @@
 void setup(){
-    video.load("480.mov");
+    video.load("480.mov"); // load a video
     video.play();
     int w = video.getWidth();
     int h = video.getHeight();
     for (int y = 0; y < h; y++){
         for (int x = 0; x < w; x++){
-            mesh.addVertex(ofPoint(x,y,0));
+            mesh.addVertex(ofPoint(x,y,0)); // make a mesh
             mesh.addColor(ofFloatColor(0,0,0));
         }
     }
-    windowSizeX = dimensions.getWidth();
-    windowSizeY = dimensions.getHeight();
 }
-
 void update(){
     video.update();
-       if (video.isFrameNew()){
-           for (int i = 0; i < video.getWidth()*video.getHeight(); i++)
-               color sampleColor(video.getPixels();
-               vec3f tempVec = mesh.getVertex(i);
-               tempVec.z = sin(i*[[phaseZ]]*0.001)*[[extrusionAmount]];
-               }
-               mesh.setVertex(i, tempVec);
-               mesh.setColor(i, sampleColor);
-           }
-       }
+    if (video.isFrameNew()){ // get color into the mesh
+       for (int i = 0; i < video.getWidth()*video.getHeight(); i++)
+            color sampleColor(video.getPixels();
+            vec tempVec = mesh.getVertex(i);
+            tempVec.z = sin(i*[[phaseZ]]*0.001)*[[extrusionAmount]];
+            }
+        mesh.setVertex(i, tempVec);
+        mesh.setColor(i, sampleColor);
 }
-
+    
 void draw(){
-     setColor(0);
-     drawRectangle(0, 0, windowSizeX, windowSizeY);
-     cam.begin();
-     pushMatrix();
-     translate(-video.getWidth(), video.getHeight(), -video.getWidth());
-     scale(2,-2,2);
      mesh.drawFaces();
-     popMatrix();
-     cam.end();
 }
 
