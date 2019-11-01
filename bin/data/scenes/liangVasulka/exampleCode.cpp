88a5fed6d3a3358f1c1f1dd1bbe8a10d9ca654e7
montoyamoraga <montoyamoraga@gmail.com>
2019-11-01 10:37:03 -0400

    add liang pseudocode

--- bin/data/scenes/liangVasulka/exampleCode.cpp
+++ bin/data/scenes/liangVasulka/exampleCode.cpp
@@ -1,4 +1,12 @@
-// code goes here!
-
-//if you want to show a parameter's value, which highlights when it changes you need to enclose it's name in double square brackets.
-// for a parameter named myParam you shoudl put [[myParam]] in the pseudo code below.
+//inspired by art work by Steina and Woody Vasulka
+//recoded by Liang Zhipeng
+for (int i = -0.66*center_x; i < 0.66*center_x; i++){
+    for (int j = -0.7*center_y; j < 0.7*center_y; j++){
+        if((i*i + j*j)< (48*center_x^2+(int)ofRandom(-(float)[[Magic Number]])+48*center_y^2)){
+           ofSeedRandom([[Speed]]);
+           ofSetColor(ofRandom(255), ofRandom(255), ofRandom(255));
+           ofDrawRectangle(i+ofRandom(-10,10), j+ofRandom(-10,10), 1, 1);
+        } else {
+            ofSetColor( ofNoise(i*0.01, j*0.01, 0.01*[[Speed]])*255,ofNoise(i*0.01, j*0.01, 0.01*[[Speed]])*255,ofNoise(i*0.01, j*0.01, 0.01*[[Speed]])*255);
+           ofDrawRectangle(i,j,1,1);
+         }}}
