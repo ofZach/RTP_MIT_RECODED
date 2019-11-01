d441c646af4dc392b44cf514f719f13151852dca
montoyamoraga <montoyamoraga@gmail.com>
2019-11-01 09:32:42 -0400

    add params to hanna sketches

--- bin/data/scenes/hannahMuriel2/exampleCode.cpp
+++ bin/data/scenes/hannahMuriel2/exampleCode.cpp
@@ -1,9 +1,9 @@
 void draw{
     
 for (int j = 0; j < 8; j=j+1){
-    ofDrawRectangle(300, 300, 540-(j-j*0.06)*59, 540-(j-j*0.06)*59);
+    ofDrawRectangle(300, 300, 540-(j-j*[[red]])*59, 540-(j-j*[[red]])*59);
     ofSetColor(48,91,216);
-    ofDrawRectangle(300, 300, 500-(j-j*0.04)*60, 500-(j-j*0.04)*60);
+    ofDrawRectangle(300, 300, 500-(j-j*[[blue]])*60, 500-(j-j*[[blue]])*60);
 }
     ofSetColor(250,30,30);
     ofDrawRectangle(300, 300, 140, 140);
--- bin/data/scenes/hannahSchwartz/exampleCode.cpp
+++ bin/data/scenes/hannahSchwartz/exampleCode.cpp
@@ -3,9 +3,25 @@ void draw(){
     int frameCount = ofGetElapsedTimef()*10;
     float time = ofGetElapsedTimef();
     
+    if (0 < [[color]] < 10){
+        ofBackground(6,28,209,150);
+    }
+    if (20 < [[color]] < 40){
+        ofBackground(193,155,232);
+    }
+    if (20 < [[color]] < 60){
+        ofBackground(115,168,219);
+    }
+    if ([60 < [[color]] < 80){
+        ofBackground(255);
+    }
+    if (80 < [[color]] < 100){
+        ofBackground(193,155,232);
+    }
+    
     ofPushMatrix();
     ofTranslate(500,300);
-    ofRotate(speed*time);
+    ofRotate([[speed]]*time);
     if (frameCount%5 ==0 | frameCount%3 ==0 | (frameCount > 50 && frameCount < 150) ){
         ofSetColor(234,229,178,200);
         if ((frameCount > 150 && frameCount < 200)){
@@ -24,7 +40,7 @@ void draw(){
     else {
         ofSetColor(209,16,6,200);
     }
-    ofRotate(-2*speed*time);
+    ofRotate(-2*[[speed]]*time);
     ofDrawEllipse(0,-90,220,180);
     ofDrawEllipse(0,-90,260,180);
     ofPopMatrix();
